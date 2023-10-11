3549cea3155e78e62c33e1f75568bfcafb78697a
notunk0wnalt <143821901+notunk0wnalt@users.noreply.github.com>
2023-10-11 17:49:30 +0000

    working on the footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,16 @@ const textLength = heading.innerText.length;
 const fontSize = 50 - (textLength * 2); // Adjust the formula as per your requirements
   
 heading.style.fontSize = `${fontSize}px`;
+
+var menuOpen = false;
+
+const menuOpenButton = document.getElementById('menuOpenButton');
+menuOpenButton.addEventListener('click', () => {
+    // Set sidebar state to hover
+    if(!menuOpen){
+        menuOpen = true;
+    } else {
+        menuOpen = false;
+    }
+    document.getElementById('sidebar').classList.toggle('hover');
+});
